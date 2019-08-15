7055af1bd35a303c6b2c89b3ecff633b4dad8e98
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-08-14 19:28:11 -0700

    worked on custom loop for the shop page

--- themes/inhabitent-redstarter-master/archive-product.php
+++ themes/inhabitent-redstarter-master/archive-product.php
@@ -13,22 +13,69 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
+			<h2>Shop Stuff</h2>
+
 			</header><!-- .page-header -->
 
+			<div class="shop-container">
+				<ul>
+				<?php 
+   
+   $namesArgs=[
+	   'taxonomy'=>'product-type',
+	   'hide_empty' => true,
+	   'order' => 'ASC',
+   ];
+
+   $names =get_terms($namesArgs);
+   foreach ( $names as $name ) {
+	   
+	
+	  echo "<li><a href=". get_term_link($name). ">" .$name->name."</a></li>"; 
+	 
+	  
+       
+
+   } ?>
+</ul>
+
+			</div>
+
+
+
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+		 <?php
+		//   while ( have_posts() ) : the_post();
+			 ?> 
+		
+			<?php
+   $args = array(
+	    'post_type' => 'product',
+		'order' => 'ASC',
+		'posts_per_page'=>-1,
+	);
+        $images = get_posts( $args );
+	?>
 
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
+	
+<?php foreach ($images as $image): setup_postdata($image); ?>
+<?php 
+ echo get_the_post_thumbnail($image->ID);
+echo ($image->post_title);
+echo '.................';
+echo '$'.CFS()-> get('product_price');
+echo get_post_permalink($image->ID);
 
-			<?php endwhile; ?>
+ ?>
 
-			<?php the_posts_navigation(); ?>
+<?php endforeach ?>
+<?php wp_reset_postdata() ?>
+
+			<?php
+			//  endwhile; 
+			?> 
+
+		
 
 		<?php else : ?>
 
